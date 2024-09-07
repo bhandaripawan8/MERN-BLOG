19047dd014d827e51506111664bc3875e8502b4d
Pawan <pawanb78@gmail.com>
2024-09-07 05:16:57 +0100

    user model, routes, controller done and tested

--- API/controller/userController.js
+++ API/controller/userController.js
@@ -1,4 +1,4 @@
-import userModel from "../models/userModel.js";
+import userModel from '../Models/User.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import validator from 'validator';
